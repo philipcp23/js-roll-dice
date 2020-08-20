7ae50f1a03daf4ed621fe014c86f75a1a12c3788
Phil Paolo <philipcpaolo@gmail.com>
2020-08-20 12:48:16 -0400

    add init function

--- src/app.js
+++ src/app.js
@@ -2,22 +2,10 @@
 */
 let scores, currentScore, activePlayer;
 
-scores = [0, 0];
-currentScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
+init();
 
 // NEW GAME
-document.querySelector('.btn-new').addEventListener('click', ()=> {
- 
-});
+document.querySelector('.btn-new').addEventListener('click', init);
 
 // ROLL DICE
 document.querySelector('.btn-roll').addEventListener('click', ()=> {    
@@ -59,6 +47,31 @@ document.querySelector('.btn-hold').addEventListener('click', ()=> {
 
 
 // functions
+function init() {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    
+
+
+}
+
+
+
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;   
     currentScore = 0;
