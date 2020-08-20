60249fab47692a3bee11ec30da2019a89992e619
Phil Paolo <philipcpaolo@gmail.com>
2020-08-20 09:44:27 -0400

    toggle active panel on hold btn event

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 /*Pig Game
 */
-let scores, roundScore, activePlayer;
+let scores, currentScore, activePlayer;
 
 scores = [0, 0];
-roundScore = 0;
-activePlayer = 1;
+currentScore = 0;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+setCurrentZero();
+setScoreZero();
 
+// NEW GAME
+document.querySelector('.btn-new').addEventListener('click', ()=> {
+    setCurrentZero();
+    setScoreZero();
+    removeDice();
+    setActivePanelForNewGame();
+ 
+});
+
+// ROLL DICE
 document.querySelector('.btn-roll').addEventListener('click', ()=> {    
     // 1. Random number
     let dice = Math.floor(Math.random() *6) + 1;
@@ -20,9 +28,58 @@ document.querySelector('.btn-roll').addEventListener('click', ()=> {
     let diceDOM = document.querySelector('.dice')
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
-    // 3. Update the round score IF the rolled number was NOT a 1
+    // 3. Update the current score IF the rolled number was NOT a 1
+    if(dice !== 1) {
+        // Add score
+        currentScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = currentScore;
+    } else {
+        // Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        setCurrentZero();
+        toggleActivePanel();
+
+
+       // document.querySelector('.player-0-panel').classList.remove('active');
+       // document.querySelector('.player-1-panel').classList.add('active');
+        removeDice();
+       
+    }
 });
 
+// HOLD
+document.querySelector('.btn-hold').addEventListener('click', ()=> {
+    toggleActivePanel();
+})
+
+
+
+
+
+// functions
+function toggleActivePanel() {
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+}
+
+function setScoreZero () {
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+}
+
+function setCurrentZero () {
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+}
+
+function removeDice() {
+    document.querySelector('.dice').style.display = 'none';
+}
+
+function setActivePanelForNewGame() {
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+}
 
 
 
@@ -31,8 +88,7 @@ document.querySelector('.btn-roll').addEventListener('click', ()=> {
 
 
 
-
-// document.querySelector('#current-' + activePlayer).textContent = dice;
+// ;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 // let x = document.querySelector('#score-0').textContent;
 
